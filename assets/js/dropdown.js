e6b7a9111327728ee80e4a4b04d9a7c97de89b8e
whoseinm <huseynmikayilzade07@gmail.com>
2022-10-04 23:58:49 +0400

    first commit

---
+++ assets/js/dropdown.js
@@ -0,0 +1,10 @@
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    })
+});     
+</script>
