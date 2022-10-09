1484d2ab9aeae1d3f25b670398a4fe5bd002ce2e
whoseinm <huseynmikayilzade07@gmail.com>
2022-10-09 21:32:36 +0400

    finished

--- assets/js/more_filter.js
+++ assets/js/more_filter.js
@@ -1,29 +1,20 @@
-document.getElementsByClassName("more_filter")[0].onclick = function() {show()};
-
-
-function show(){
-    document.getElementById("hided").style.display = "block"
-    document.getElementsByClassName("more_filter")[0].innerHTML = "Gizlət"
-};
-
-document.getElementsByClassName("more_filter")[0].ondblclick = function() {hide()};
-
-
-function hide(){
-    document.getElementById("hided").style.display = "none"
-    document.getElementsByClassName("more_filter")[0].innerHTML = "Daha çox filtr"
-};
-
+$(document).ready(function () {
+    $("#more_filter").click(function () {
+        $("#hided").toggleClass("hidden");
+        
+    });
+});
 
 function myFunction(x) {
     x.classList.toggle("fa-solid");
 }
 
-(function() {
+(function () {
     'use strict';
-  
+
     let style = document.createElement('style');
     style.innerHTML = '*{ user-select: none !important; }';
-  
+
     document.body.appendChild(style);
 })();
+
